5ba65a8a914186e5e4c7c487de88ee6b52b7d302
aldomunaretto <aldomunaretto@gmail.com>
2024-10-19 08:43:11 +0200

    Uploading contracts from the syntax sessions of the Onchain Builder Lab course | Part 2A - Basic Solidity

---
+++ contracts/EducaETH/README.md
@@ -0,0 +1,5 @@
+## Mastering Ethereum
+https://github.com/ethereumbook/ethereumbook
+
+## Repositorio prácticas del Curso Onchain Builder Lab | Parte 2A - Solidity Básico
+https://github.com/Oriojas/solidity_basico_educateth
---
+++ contracts/EducaETH/hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;  // en esta parte definoi la version del compilador
+
+// a partir de aca coloco la logica de mi contrato
+contract hola_mundo {
+
+    string public saludo = "Hola desde Educateth 2.0"; // declarando el contenido de mi variable saludo
+
+}
